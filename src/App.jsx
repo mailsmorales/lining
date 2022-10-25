1f4c9b58871679b31afd3ffe0027a3561c5d206f
mukhamed-Ali <mailsmorales21@mail.ru>
2022-10-25 18:38:10 +0600

    added component basegooglebt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { BaseButton } from "./components/ui/button/BaseButton";
+import { BaseGoogleBt } from "./components/ui/button/BaseGoogleBt"
 
 function App() {
   return (
-    <div>
-      <BaseButton name={"Подробнее"} withIcon iconType="right" />
-    </div>
+    <>
+      <div>
+        <BaseButton name={"Подробнее"} withIcon iconType="right" />
+      </div>
+        <BaseGoogleBt name={"Войти через Google"} />
+    </>
   );
 }
 
--- src/components/ui/button/BaseButton.jsx
+++ src/components/ui/button/BaseButton.jsx
@@ -9,7 +9,7 @@ const frRightIcon = (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      className=" fill-white group-hover:fill-indigo-400"
+      className="fill-white group-hover:fill-red-600"
       d="M10.9084 0.758337C10.5834 1.08334 10.5834 1.60834 10.9084 1.93334L14.1417 5.16667H1.50008C1.04175 5.16667 0.666748 5.54167 0.666748 6C0.666748 6.45834 1.04175 6.83334 1.50008 6.83334H14.1501L10.9167 10.0667C10.5917 10.3917 10.5917 10.9167 10.9167 11.2417C11.2417 11.5667 11.7667 11.5667 12.0917 11.2417L16.7501 6.58334C17.0751 6.25834 17.0751 5.73334 16.7501 5.40834L12.0834 0.758337C11.7667 0.433337 11.2334 0.433337 10.9084 0.758337Z"
       fill="white"
     />
@@ -24,7 +24,7 @@ const frSouthIcon = (
     xmlns="http://www.w3.org/2000/svg"
   >
     <g clip-path="url(#clip0_8_127)">
-      <path
+      <path className=" fill-white group-hover:fill-red-600"
         d="M18 9C17.44 9 17 9.45 17 10V15.59L6.11998 4.7C5.72998 4.31 5.09998 4.31 4.70998 4.7C4.31998 5.09 4.31998 5.72 4.70998 6.11L15.59 17H9.99998C9.44998 17 8.99998 17.45 8.99998 18C8.99998 18.55 9.44998 19 9.99998 19H18C18.55 19 19 18.55 19 18V10C19 9.45 18.55 9 18 9Z"
         fill="#121720"
       />
@@ -45,7 +45,7 @@ const frUpIcon = (
     xmlns="http://www.w3.org/2000/svg"
   >
     <g clip-path="url(#clip0_8_82)">
-      <path
+      <path className=" fill-white group-hover:fill-red-600"
         d="M5.70998 9.7C6.09998 10.09 6.72998 10.09 7.11998 9.7L11 5.83V21C11 21.55 11.45 22 12 22C12.55 22 13 21.55 13 21V5.83L16.88 9.71C17.27 10.1 17.9 10.1 18.29 9.71C18.68 9.32 18.68 8.69 18.29 8.3L12.7 2.7C12.31 2.31 11.68 2.31 11.29 2.7L5.70998 8.29C5.31998 8.68 5.31998 9.32 5.70998 9.7Z"
         fill="#121720"
       />
@@ -61,12 +61,12 @@ const frUpIcon = (
 export const BaseButton = ({ name, withIcon, iconType }) => {
   return (
     <div>
-      <button class="flex text-center items-center group px-8 py-4 rounded-lg border-2 bg-indigo-400 hover:bg-white hover:border-indigo-400">
-        <span class="text-white group-hover:text-indigo-400 text-base font-medium">
+      <button className="flex text-center items-center group px-8 py-4 rounded-lg border-2 bg-red-600 hover:bg-white hover:border-red-600">
+        <span className="text-white group-hover:text-red-600 text-base font-medium">
           {name}
         </span>
         {withIcon && iconType && (
-          <div class="flex items-center ml-2">
+          <div className="flex items-center ml-2">
             {iconType === "right" && frRightIcon}
             {iconType === "south_east" && frSouthIcon}
             {iconType === "norht" && frUpIcon}
