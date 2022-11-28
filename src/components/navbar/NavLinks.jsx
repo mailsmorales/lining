f5ff163fb536e195941d4abc89d85e275fadf524
mukhamed-Ali <mailsmorales21@mail.ru>
2022-11-28 09:45:39 +0600

    added MyLinks component for NavLinks

--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -1,14 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { links } from "./MyLinks";
 
 export const NavLinks = () => {
-  const links = [{ name: "Men" }, { name: "Women" }, { name: "Kid's" }];
   return (
     <>
       {links.map((link) => (
         <div>
-          <div className="px-3 text-left md:cursor-pointer">
+          <div className="px-3 text-left md:cursor-pointer group">
             <h1 className="py-7">{link.name}</h1>
+            {link.submenu && (
+              <div>
+                <div className=" absolute top-20 hidden group-hover:block hover:block">
+                  <div className="py-3">
+                    <div className="w-4 h-4 left-3 absolute mt-1 bg-purple-700 rotate-45"></div>
+                  </div>
+                  <div className=" bg-purple-700 p-5 grid grid-cols-3 gap-10">
+                    {link.sublinks.map((mysublinks) => (
+                      <div>
+                        <h1 className="text-lg font-semibold">
+                          {mysublinks.Head}
+                        </h1>
+                        {mysublinks.sublink.map((slink) => (
+                          <li className="text-sm my-2.5">
+                            <Link
+                              to={slink.link}
+                              className="hover:text-purple-200"
+                            >
+                              {slink.name}
+                            </Link>
+                          </li>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
+          
         </div>
       ))}
     </>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/img/Logo.png";
 import { BaseButton } from "../ui/button/BaseButton";
 import { NavLinks } from "./NavLinks";
 
 export const NavBar = () => {
-  
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="bg-purple-700">
       <div className="flex items-center font-medium justify-around">
         <div className="z-50 p-5 md:w-auto w-full flex justify-between">
           <img src={Logo} alt="logo" className="md:cursor-pointer h-9" />
-          <div className="text-3xl">
-          <ion-icon name="menu"></ion-icon>
+          <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
+            <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
           </div>
         </div>
         <ul className="md:flex hidden uppercase items-center gap-8 text-white">
@@ -27,7 +28,11 @@ export const NavBar = () => {
           <BaseButton name="Войти" />
         </div>
         {/* Mobile nav */}
-        <ul className={`md:hidden bg-purple-700 absolute w-full h-full bottom-0 py-24 pl-4 text-white`}>
+        <ul
+          className={`md:hidden bg-purple-700 absolute w-full h-full bottom-0 py-24 pl-4 text-white duration-500 ${
+            open ? "left-0" : "left-[-100%]"
+          }`}
+        >
           <li>
             <Link to="/" className="py-7 px-3 inline-block">
               Главная
@@ -35,7 +40,7 @@ export const NavBar = () => {
           </li>
           <NavLinks />
           <div className="py-5">
-          <BaseButton name="Войти" />
+            <BaseButton name="Войти" />
           </div>
         </ul>
       </div>
